3211e17f5c072abe4131c5a5a270bd30e3fc7f73
kcresswell <makayla.cresswell@gmail.com>
2017-11-17 10:33:31 -0700

    added to mergesort, also added insertionsort() method to use in mergesort

--- CS6012/QuicksortAndMergesort/src/assignment04/SortUtil.java
+++ CS6012/QuicksortAndMergesort/src/assignment04/SortUtil.java
@@ -5,18 +5,51 @@ import java.util.Comparator;
 
 public class SortUtil {
 	
-	
+	/**
+	 * 
+	 * @param stringToSort
+	 * @return
+	 */
+	public static String insertionSort(String stringToSort) {
+		// convert the array to characters, must be lowercase for proper comparison
+		char[] strArray = stringToSort.toLowerCase().toCharArray();
+
+		// loop through the array and perform insertion sort
+		for (int i = 1; i < strArray.length; i++) {
+			char index = strArray[i];
+			int j = i;
+
+			while (j > 0 && strArray[j - 1] > index) {
+				strArray[j] = strArray[j - 1];
+				j--;
+			}
+			strArray[j] = index;
+		}
+		// convert the array back to a string and return
+		return new String(strArray);
+	}
 	
 	public static <T> void mergesort(ArrayList<T> myArrayList, Comparator<? super T> comparator){
-
-	//This method performs a mergesort on the generic ArrayList given as input.
-
-	//For the mergesort algorithm, see the class notes and/or the textbook. 
-	//Your mergesort implementation must switch over to insertion sort when 
-	//the size of the sublist to be sorted meets a certain threshold (i.e., becomes small enough). 
-	//Make this threshold value a private static variable that you can easily change.
-	//You will perform experiments to determine which threshold value works best (see the Analysis Document).
-	//Don't forget to include the insertion sort in the program files you submit.
+		//need to call insertionSort() method somewhere in here, may have to reconfigure the code a bit
+	  
+	  /*
+	   * FROM HIS SLIDES, pseudocode
+	   void MergeSort(int[] arr, int start, int end) {
+	   // arrays of size 1 are already sortedif(start >= end) {
+	    *  return; 
+	    *  }
+	    *  int mid = (start + end) /2;
+	    *  MergeSort(arr, start, mid); 
+	    *  // left halfMergeSort(arr, mid+1, end); 
+	    *  // right halfMerge(arr, start, mid, end); 
+	    *  //merge halves}
+	    *  
+	    *  ///NEXT METHOD
+	    *  void Merge(int[] arr, int[] temp, int start, int mid, int end){
+	    *  int i1 = 0, i2 = mid;while(i1 < mid && i2 < end){
+	    *  put smaller of arr[i1], arr[i2] in to temp increment appropriate i}
+	    *  copy anything left over from larger half to tempcopy temp from (start to end) back int to arr (from start to end) }
+	   */
 		
 	}
 
