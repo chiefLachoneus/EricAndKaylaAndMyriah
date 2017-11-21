7ec1f180bc8b44f62cd3731087fbfc77a1a56292
kcresswell <makayla.cresswell@gmail.com>
2017-11-21 09:57:59 -0700

    edited test file

--- CS6012/QuicksortAndMergesort/src/assignment04/TestQuicksortAndMergeSort.java
+++ CS6012/QuicksortAndMergesort/src/assignment04/TestQuicksortAndMergeSort.java
@@ -45,7 +45,41 @@ public class TestQuicksortAndMergeSort {
     sortedNumbers.add(12);
     sortedNumbers.add(35);
     
+    arrayListOfInts.add(10);
+    arrayListOfInts.add(8);
+    arrayListOfInts.add(7);
+    arrayListOfInts.add(3);
+    arrayListOfInts.add(5);
+    arrayListOfInts.add(6);
+    arrayListOfInts.add(1);
+    arrayListOfInts.add(2);
+    arrayListOfInts.add(4);
+    arrayListOfInts.add(9);
+    arrayListOfInts.add(11);
     
+    sortedArrayListOfInts.add(1); 
+    sortedArrayListOfInts.add(2); 
+    sortedArrayListOfInts.add(3); 
+    sortedArrayListOfInts.add(4); 
+    sortedArrayListOfInts.add(5); 
+    sortedArrayListOfInts.add(6); 
+    sortedArrayListOfInts.add(7); 
+    sortedArrayListOfInts.add(8); 
+    sortedArrayListOfInts.add(9); 
+    sortedArrayListOfInts.add(10); 
+    sortedArrayListOfInts.add(11); 
+    
+    bigIntArray.add(2147483647);
+    bigIntArray.add(2147483500); 
+    bigIntArray.add(2147483540); 
+    bigIntArray.add(2147483550);
+    bigIntArray.add(2147483600); 
+    
+    sortedBigIntArray.add(2147483500);
+    sortedBigIntArray.add(2147483540);
+    sortedBigIntArray.add(2147483550);
+    sortedBigIntArray.add(2147483600);
+    sortedBigIntArray.add(2147483647);
   }
   
   @Test
@@ -89,55 +123,14 @@ public class TestQuicksortAndMergeSort {
   }
   
   @Test
-  public void testMergesort() {
-    arrayListOfInts.add(10);
-    arrayListOfInts.add(8);
-    arrayListOfInts.add(7);
-    arrayListOfInts.add(3);
-    arrayListOfInts.add(5);
-    arrayListOfInts.add(6);
-    arrayListOfInts.add(1);
-    arrayListOfInts.add(2);
-    arrayListOfInts.add(4);
-    arrayListOfInts.add(9);
-    arrayListOfInts.add(11);
-    
-    sortedArrayListOfInts.add(1); 
-    sortedArrayListOfInts.add(2); 
-    sortedArrayListOfInts.add(3); 
-    sortedArrayListOfInts.add(4); 
-    sortedArrayListOfInts.add(5); 
-    sortedArrayListOfInts.add(6); 
-    sortedArrayListOfInts.add(7); 
-    sortedArrayListOfInts.add(8); 
-    sortedArrayListOfInts.add(9); 
-    sortedArrayListOfInts.add(10); 
-    sortedArrayListOfInts.add(11); 
-    
-    
-    
-    // public static <T> void mergesort(ArrayList<T> myArrayList, Comparator<? super T> comparator)
-    
+  public void testMergesort() {    
     SortUtil.mergesort(arrayListOfInts, new IntComparator());
     
     for(int i = 0; i < arrayListOfInts.size(); i++) {
       System.out.println(arrayListOfInts.get(i));
       assertTrue(arrayListOfInts.get(i).equals(sortedArrayListOfInts.get(i)));
-    }
-    
-    
+    }  
   }
-  
-//  @Test
-//  public void testNullArrayList() {
-//    nullArrayList.add(null); 
-//    nullArrayList.add(null);
-//    
-//    for(int i = 0; i < nullArrayList.size(); i++) {
-//      assertTrue(nullArrayList.get(i).equals(nullArrayList.get(i)));
-//    }
-//    
-//  }
   
   @Test
   public void testOneInt() {
@@ -152,23 +145,22 @@ public class TestQuicksortAndMergeSort {
   
   @Test
   public void testBigInt() {
-    bigIntArray.add(2147483647);
-    bigIntArray.add(2147483500); 
-    bigIntArray.add(2147483540); 
-    bigIntArray.add(2147483550);
-    bigIntArray.add(2147483600); 
-    
-    sortedBigIntArray.add(2147483500);
-    sortedBigIntArray.add(2147483540);
-    sortedBigIntArray.add(2147483550);
-    sortedBigIntArray.add(2147483600);
-    sortedBigIntArray.add(2147483647);
-    
     SortUtil.mergesort(bigIntArray, new IntComparator());
     
     for(int i = 0; i < bigIntArray.size(); i++) {
       assertTrue(bigIntArray.get(i).equals(sortedBigIntArray.get(i)));
     }
   }
+  
+//@Test
+//public void testNullArrayList() {
+//  nullArrayList.add(null); 
+//  nullArrayList.add(null);
+//  
+//  for(int i = 0; i < nullArrayList.size(); i++) {
+//    assertTrue(nullArrayList.get(i).equals(nullArrayList.get(i)));
+//  }
+//  
+//}
   
 }
