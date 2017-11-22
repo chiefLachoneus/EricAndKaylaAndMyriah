9a2b57bd8c39889db49a78cbab091f363d2e0c60
Eric <truthdefender02@yahoo.com>
2017-11-21 23:03:34 -0700

    updated testing sinerios.

--- CS6012/QuicksortAndMergesort/src/assignment04/SortDriver.java
+++ CS6012/QuicksortAndMergesort/src/assignment04/SortDriver.java
@@ -9,52 +9,42 @@ public class SortDriver {
     
     // First, spin computing stuff until one second has gone by.
     // This allows this thread to stabilize.
-    //int wordCount = 3000;
-    // String[] testInput = generateRandomString(11, wordCount);
+    ArrayList<Integer> inputSizes = new ArrayList<Integer>();
     
-    for(int inputSize = 100; inputSize < 1000; inputSize+=100){
+    for (int inputSize = 1000; inputSize <= 100000; inputSize += 1000) {
       
       ArrayList<Integer> originalArray = SortUtil.generateWorstCase(inputSize);
-      
-      
-      
-      System.out.println("Input");
-      System.out.println(inputSize);
-      
-      
-      
+      inputSizes.add(inputSize);
       
       startTime = System.nanoTime();
       while (System.nanoTime() - startTime < 1000000000) {
       }
       
       // Now, run the test.
-      
       long timesToLoop = 10;
-      
       
       totalTime = 0;
       
       for (long i = 0; i < timesToLoop; i++) {
         ArrayList<Integer> copiedArray = new ArrayList<Integer>();
-        for(int j = 0; j < originalArray.size(); j++){
+        for (int j = 0; j < originalArray.size(); j++) {
           copiedArray.add(originalArray.get(j));
           
         }
         startTime = System.nanoTime();
-        SortUtil.quicksort(copiedArray, new NumberComparator());
-        //SortUtil.mergesort(copiedArray, new NumberComparator(), 15);
+//        SortUtil.quicksort(copiedArray, new NumberComparator());
+         SortUtil.mergesort(copiedArray, new NumberComparator(), 2);
         stopTime = System.nanoTime();
         totalTime += stopTime - startTime;
       }
       
-      double averageTime = (double)totalTime / timesToLoop;
-      //System.out.println("Threshold");
-      //System.out.println(threshold);
+      double averageTime = (double) totalTime / timesToLoop;
+      // System.out.println("Threshold");
+      // System.out.println(threshold);
       System.out.println(averageTime);
-      
-      
-      System.out.println();
+    }
+    for(Integer number: inputSizes) {
+      System.out.println(number);
     }
   }
   
